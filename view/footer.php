bd534ab8e72f979e706b72436b6d0c69f164315e
Dany Vanesa Ruiz Pinilla <davaruiz.02@gmail.com>
2023-10-22 19:18:03 -0500

    Ajustar Footer con estilos css

--- view/footer.php
+++ view/footer.php
@@ -1,21 +1,28 @@
 <!DOCTYPE html>
 <html lang="es">
 
+
 <body>
+    
     <footer>
-        <div class="empresa-info">
-            <h2>Nombre de la Empresa</h2>
-            <p>Dirección de la Empresa</p>
-            <p>Teléfono: (123) 456-7890</p>
-            <p>Email: info@empresa.com</p>
+        <h2>Milkcollector</h2>
+        <div class="columnas-footer">
+            <div class="empresa-info">
+                <p>Dirección de la Empresa</p>
+                <p>Teléfono: (123) 456-7890</p>
+                <p>Email: info@empresa.com</p>
+            </div>
+            <div class="redes-sociales">
+                <a href="https://www.facebook.com" target="_blank">
+                    <img src="view/img/facebook.png" alt="Facebook">
+                </a>
+                <a href="https://wa.me/+573002750649" target="_blank">
+                    <img src="view/img/whatsapp.png" alt="WhatsApp">
+                </a>
+            </div>
         </div>
-        <div class="redes-sociales">
-            <a href="https://www.facebook.com" target="_blank">
-                <img src="view/img/facebook.png" alt="Facebook">
-            </a>
-            <a href="https://wa.me/+573002750649" target="_blank">
-                <img src="view/img/whatsapp.png" alt="WhatsApp">
-            </a>
+        <div class="derechos-reservados">
+            <p>&copy; 2023 Nombre de la Empresa. Todos los derechos reservados.</p>
         </div>
     </footer>
 </body>
